db177a9442652e5d04d0665e42a126ad26270b48
Sven Verboven <sven_verboven@hotmail.com>
2020-05-11 19:04:41 +0200

    added tests

---
+++ youcoach-backend/src/test/java/com/switchfully/youcoach/api/endpoints/UsersControllerTest.java
@@ -0,0 +1,123 @@
+package com.switchfully.youcoach.api.endpoints;
+
+import com.switchfully.youcoach.domain.user.UsersRepository;
+import com.switchfully.youcoach.security.authentication.user.SecuredUserRepository;
+import com.switchfully.youcoach.service.dto.CreateUserDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UsersControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private SecuredUserRepository securedUserRepository;
+
+    @Test
+    void register_givenCreateUserDto_thenResponseStatusIsCreated() {
+        CreateUserDto createUserDto = new CreateUserDto("test@gmail.com", "Password1", "first name", "last name");
+
+        webTestClient.post()
+                .uri(UsersController.USERS_RESOURCE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createUserDto), CreateUserDto.class)
+                .exchange()
+                .expectStatus().isCreated();
+    }
+
+    @Test
+    void register_givenWrongEmailFormatWithNoAt_thenResponseStatusIsBadRequest() {
+        CreateUserDto createUserDto = new CreateUserDto("testgmail.com", "Password1", "first name", "last name");
+
+        webTestClient.post()
+                .uri(UsersController.USERS_RESOURCE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createUserDto), CreateUserDto.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void register_givenWrongEmailFormatWithNoPoint_thenResponseStatusIsBadRequest() {
+        CreateUserDto createUserDto = new CreateUserDto("test@gmailcom", "Password1", "first name", "last name");
+
+        webTestClient.post()
+                .uri(UsersController.USERS_RESOURCE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createUserDto), CreateUserDto.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void register_givenWrongPasswordFormatWithNoCapitalLetter_thenResponseStatusIsBadRequest() {
+        CreateUserDto createUserDto = new CreateUserDto("test@gmail.com", "password1", "first name", "last name");
+
+        webTestClient.post()
+                .uri(UsersController.USERS_RESOURCE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createUserDto), CreateUserDto.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void register_givenWrongPasswordFormatWithLengthUnderEight_thenResponseStatusIsBadRequest() {
+        CreateUserDto createUserDto = new CreateUserDto("test@gmail.com", "Pass1", "first name", "last name");
+
+        webTestClient.post()
+                .uri(UsersController.USERS_RESOURCE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createUserDto), CreateUserDto.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void register_givenWrongPasswordFormatWithNoNumber_thenResponseStatusIsBadRequest() {
+        CreateUserDto createUserDto = new CreateUserDto("test@gmail.com", "Password", "first name", "last name");
+
+        webTestClient.post()
+                .uri(UsersController.USERS_RESOURCE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createUserDto), CreateUserDto.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    void register_givenEmailThatIsAlreadyRegistered_thenResponseStatusIsBadRequest() {
+        CreateUserDto createUserDto = new CreateUserDto("test@gmail.com", "Password1", "first name", "last name");
+
+        webTestClient.post()
+                .uri(UsersController.USERS_RESOURCE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createUserDto), CreateUserDto.class)
+                .exchange()
+                .expectStatus().isCreated();
+
+        webTestClient.post()
+                .uri(UsersController.USERS_RESOURCE_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(createUserDto), CreateUserDto.class)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @AfterEach
+    void tearDown() {
+        usersRepository.deleteAll();
+        securedUserRepository.deleteAll();
+    }
+}
