52f2d0afc04daba1f0b84e1e3e635cf5c8f22993
nielsh <Niemand@01>
2020-05-12 13:14:01 +0200

    get user info by id

--- youcoach-backend/src/main/java/com/switchfully/youcoach/domain/user/UsersRepository.java
+++ youcoach-backend/src/main/java/com/switchfully/youcoach/domain/user/UsersRepository.java
@@ -1,9 +1,9 @@
 package com.switchfully.youcoach.domain.user;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.UUID;
+
 public interface UsersRepository extends CrudRepository<Users, Long> {
-    @Query ("select s.securedUser from Users s where s.securedUser.username = ?1")
-    Users findBySecuredUser_Username (String username);
+    Users findBySecuredUser_Id (UUID id);
 }
